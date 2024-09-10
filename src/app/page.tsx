ede7decfc5135b3e79d277a5ab95370e0a4cc67a
恒成立 <wo51study@163.com>
2024-09-10 22:42:30 +0800

    [需求] UUID生成器

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,95 @@
-import Image from "next/image";
+"use client";
+
+import {useState} from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const [list, setList] = useState<string[]>([]);
+    const [count, setCount] = useState<number>(1);
+    const [selectedOption, setSelectedOption] = useState<string>("lowerCase");
+    const [copied, setCopied] = useState<boolean>(false);
+
+    const handleChange = (e) => {
+        setCount(e.target.value);
+    }
+
+    function uuid() {
+        var s = [];
+        var hexDigits = "0123456789abcdef";
+        for (var i = 0; i < 36; i++) {
+            s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+        s[8] = s[13] = s[18] = s[23] = "-";
+        return selectedOption === "upperCase" ? s.join("").toUpperCase() : s.join("").toLowerCase();
+    }
+
+    const batchGenerateUUID = () => {
+        let arr = [];
+        for (let i = 0; i < count; ++i) {
+            arr = [...arr, uuid()];
+        }
+        clearUUID();
+        setList(arr);
+    }
+
+    const generateUUID = () => {
+        const u1 = uuid();
+        setList(Array.of(...list, u1));
+    }
+
+    const clearUUID = () => {
+        setList([]);
+    }
+
+    const copyUUID = async () => {
+        try {
+            await navigator.clipboard.writeText(Array.of(...list).join("\n"));
+            alert("复制成功！");
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000); // 2秒后重置状态
+        } catch (err) {
+            console.error('无法复制文本: ', err);
+        }
+    }
+
+    return (
+        <div
+            className="mx-64 flex flex-col text-center mt-32 gap-y-4 justify-between item-center">
+            <h2 className={"text-4xl text-center"}>UUID生成器</h2>
+            <div className={"flex flex-row justify-between"}>
+                <div className={"flex flex-row gap-x-4"}>
+                    <input type="text" className={"border-slate-200 border-2 rounded-md p-2"}
+                           value={count} onChange={handleChange}/>
+                    <form className={"flex flex-col justify-center items-center gap-x-4"}>
+                        <label>
+                            <input type="radio"
+                                   value={"upperCase"}
+                                   checked={selectedOption === 'upperCase'}
+                                   onChange={e => setSelectedOption(e.target.value)}/>
+                            大写
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                value={"lowerCase"}
+                                checked={selectedOption === 'lowerCase'}
+                                onChange={e => setSelectedOption(e.target.value)}/>
+                            小写
+                        </label>
+                    </form>
+                    <button onClick={batchGenerateUUID} className={"px-2 py-3 rounded-md bg-slate-300/20 w-32"}>生成</button>
+                    <button onClick={generateUUID} className={"px-2 py-3 rounded-md bg-slate-300/20 w-32"}>增加</button>
+                </div>
+                <div className={"flex flex-row justify-between gap-x-4"}>
+                    <button onClick={copyUUID} className={"px-2 py-3 rounded-md bg-slate-300/20 w-32"}>复制</button>
+                    <button onClick={clearUUID} className={"px-2 py-3 rounded-md bg-slate-300/20 w-32"}>清除</button>
+                </div>
+            </div>
+            <div className={"p-4 bg-slate-200/20 rounded-md"}>
+                {list.map(k => (<p>{k}</p>))}
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
